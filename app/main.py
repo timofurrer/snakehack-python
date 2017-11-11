d2c5d47d39e972d3f28a5a9bb4b89d4aa6a2ef6f
Timo Furrer <tuxtimo@gmail.com>
2017-11-11 10:02:17 +0100

    Change color and name

--- app/main.py
+++ app/main.py
@@ -23,10 +23,10 @@ def start():
     # TODO: Do things with data
 
     return {
-        'color': '#00FF00',
+        'color': '#910052',
         'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
         'head_url': head_url,
-        'name': 'snakehack-python'
+        'name': 'William Snakespeare'
     }
 
 
